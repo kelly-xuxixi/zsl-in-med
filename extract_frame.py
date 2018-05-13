fe5121f11c56d868efb784804238d996eb72705f
kelly-xuxixi <nflsxcx@126.com>
2018-05-13 14:18:14 +0800

    n

--- extract_frame.py
+++ extract_frame.py
@@ -23,21 +23,21 @@ def extract_frame(video_name):
     to_read = 0
     try:
         videogen = skvideo.io.vread(video_path)
+        videometadata = skvideo.io.ffprobe(video_path)
+        frame_rate = videometadata['video']['@r_frame_rate']
+        frame_rate = int(frame_rate.split('/')[0]) / int(frame_rate.split('/')[1])
+        num_frames = np.int(videometadata['video']['@nb_frames'])
+        print('  total frames: ', num_frames)
+        print('  frame rate: ', frame_rate)
+        while to_read < num_frames:
+            frame = cv2.cvtColor(videogen[to_read], cv2.COLOR_BGR2RGB)
+            cv2.imwrite(os.path.join(frame_folder_path, 'frame_' + str(to_read) + '.jpg'), frame)
+            to_read = to_read + 2 * frame_rate
     except:
         file = open('error.txt', 'a')
         file.write(video_name + '\n')
         file.close()
         return
-    videometadata = skvideo.io.ffprobe(video_path)
-    frame_rate = videometadata['video']['@r_frame_rate']
-    frame_rate = int(frame_rate.split('/')[0]) / int(frame_rate.split('/')[1])
-    num_frames = np.int(videometadata['video']['@nb_frames'])
-    print('  total frames: ', num_frames)
-    print('  frame rate: ', frame_rate)
-    while to_read < num_frames:
-        frame = cv2.cvtColor(videogen[to_read], cv2.COLOR_BGR2RGB)
-        cv2.imwrite(os.path.join(frame_folder_path, 'frame_' + str(to_read) + '.jpg'), frame)
-        to_read = to_read + 2 * frame_rate
 
 
 def main():
