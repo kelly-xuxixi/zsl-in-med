2488ed679b462215ea002124ee9ce66faa081e43
kelly-xuxixi <nflsxcx@126.com>
2018-05-15 15:06:19 +0800

    n

--- get_rank.py
+++ get_rank.py
@@ -1,4 +1,5 @@
 import numpy as np
+import util
 
 
 def get_rank():
@@ -8,13 +9,17 @@ def get_rank():
     print(key_importance.shape, key_concept_similarity.shape, probs.shape)
     key_importance.shape = (10, 1)
     key_importance = np.transpose(key_importance)
-    probs = np.transpose(probs)
-    print(key_importance.shape, key_concept_similarity.shape, probs.shape)
+    tran_probs = np.transpose(probs)
+    print(key_importance.shape, key_concept_similarity.shape, tran_probs.shape)
     result = np.dot(key_importance, key_concept_similarity)
-    result = np.dot(result, probs)
+    result = np.dot(result, tran_probs)
     print(result.shape)
     result = np.argsort(result[0])[::-1]
     print(result[:10])
+    video_name = open('event_video_list.txt', 'r').readlines()
+    for id in result[:10]:
+        print(video_name[id])
+        util.print_prob(probs[id], './synset.txt')
 
 
 if __name__ == '__main__':
--- util.py
+++ util.py
@@ -2,6 +2,7 @@ import os
 from itertools import chain
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
+import numpy as np
 
 
 def get_words_from_sentences(query):
@@ -31,6 +32,22 @@ def get_word_list_from_corpus():
     for word in word_list:
         file.write(word + ' ')
     file.close()
+
+
+# returns the top1 string
+def print_prob(prob, file_path):
+    synset = [l.strip() for l in open(file_path).readlines()]
+
+    # print prob
+    pred = np.argsort(prob)[::-1]
+
+    # Get top1 label
+    top1 = synset[pred[0]]
+    print(("Top1: ", top1, prob[pred[0]]))
+    # Get top5 label
+    top5 = [(synset[pred[i]], prob[pred[i]]) for i in range(5)]
+    print(("Top5: ", top5))
+    return top1
 
 
 # based on https://stackoverflow.com/questions/2352181/how-to-use-a-dot-to-access-members-of-dictionary
