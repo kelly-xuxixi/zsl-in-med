baf6a2493610cc99a1d1fb06e2d2ff6889ebd00c
kelly-xuxixi <nflsxcx@126.com>
2018-05-16 22:15:36 +0800

    n

--- get_mAP.py
+++ get_mAP.py
@@ -4,6 +4,7 @@ from sklearn.metrics import average_precision_score
 
 y_true = sio.loadmat('/m/data/med/metadata/y_train.mat')
 y_true = np.array(y_true['y_train'])
+y_true = np.vstack([y_true, np.zeros((4992, 20))])
 y_rank = np.loadtxt('all_ranks.txt')
 y_rank = np.transpose(y_rank)
 print(y_true.shape, y_rank.shape)
