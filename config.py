8b6cfb932e9d72629ce1287e66aa9331b1f2b7cd
kelly-xuxixi <nflsxcx@126.com>
2018-05-21 22:28:25 +0800

    n

--- config.py
+++ config.py
@@ -7,13 +7,13 @@ cfg.use_tfidf = True
 cfg.unify_synonyms = True   #
 cfg.use_log_in_word_importance = True   #
 cfg.delete_not_nones = True
-cfg.delete_ambivalent_words = True   #
+cfg.delete_ambivalent_words = False   #
 cfg.set_threshold = True
 cfg.threshold = 2.1
 # get concepts
-cfg.use_word2vec = False   #
+cfg.use_word2vec = True   #
 cfg.use_square_in_concept_selection = False
 cfg.select_concepts = True   #
-cfg.concept_threshold = 0.45
+cfg.concept_threshold = 0.7
 # evaluate
 cfg.eval_with_bg = False
