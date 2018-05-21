f265460bfe6e1b694b32045366c2a56c0b2c2c7c
kelly-xuxixi <nflsxcx@126.com>
2018-05-21 15:37:54 +0800

    n

--- get_mAP.py
+++ get_mAP.py
@@ -9,5 +9,6 @@ if cfg.eval_with_bg:
     y_true = np.vstack([y_true, np.zeros((4992, 20))])
 y_rank = np.loadtxt('all_ranks.txt')
 y_rank = np.transpose(y_rank)
+print(np.isnan(y_true).any(), np.isnan(y_rank).any())
 print(y_true.shape, y_rank.shape)
 print(average_precision_score(y_true, y_rank))
