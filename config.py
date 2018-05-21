743bfea764caa3881d5197391836e4deaa286548
kelly-xuxixi <nflsxcx@126.com>
2018-05-21 12:46:51 +0800

    n

--- config.py
+++ config.py
@@ -4,16 +4,16 @@ cfg = Dict()
 cfg.test = True
 # get key words
 cfg.use_tfidf = True
-cfg.unify_synonyms = True
-cfg.use_log_in_word_importance = True
+cfg.unify_synonyms = True   #
+cfg.use_log_in_word_importance = True   #
 cfg.delete_not_nones = True
-cfg.delete_ambivalent_words = True
+cfg.delete_ambivalent_words = True   #
 cfg.set_threshold = True
 cfg.threshold = 2.1
 # get concepts
-cfg.use_word2vec = True
+cfg.use_word2vec = True   #
 cfg.use_square_in_concept_selection = False
-cfg.select_concepts = True
+cfg.select_concepts = True   #
 cfg.concept_threshold = 0.45
 # evaluate
 cfg.eval_with_bg = False
